4be32a9df4b05d615fe65eedd8a0de003edc6194
Ivan Tetsuo <ivantetsuo@gmail.com>
2026-06-01 01:26:28 +0900

    added transaction display and balance calculation

--- BalanceManager.py
+++ BalanceManager.py
@@ -1,3 +1,18 @@
+def show_transactions():
+    for item in transactions:
+        print("="*20)
+        print(f"{item['type']} \nAmount: {item['amount']} \nDescription: {item['description']}")
+        print("="*20)
+
+def calculate_balance(transactions):
+    balance = 0
+    for item in transactions:
+        if item['type'] == 'income':
+                balance += item['amount']
+        else:
+                balance -= item['amount']
+    return balance
+
 transactions = [
 {
 'type' : 'income',
@@ -16,7 +31,6 @@ transactions = [
 }
 ]
 
-for item in transactions:
-    print("="*20)
-    print(f"{item['type']} \nAmount: {item['amount']} \nDescription: {item['description']}")
-    print("="*20)
+show_transactions()
+current_balance = calculate_balance(transactions)
+print(current_balance)
